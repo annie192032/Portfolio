4ab184a6b656049f09e00501cf2df2fdd56331fc
annie192032 <39673275+annie192032@users.noreply.github.com>
2020-03-23 14:46:51 +0800

    視窗程式

---
+++ 視窗-擊球/Final_project/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Final_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.picLine = new System.Windows.Forms.PictureBox();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.playground = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picLine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.playground.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 50;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // picLine
+            // 
+            this.picLine.BackColor = System.Drawing.Color.Black;
+            this.picLine.Location = new System.Drawing.Point(153, 469);
+            this.picLine.Name = "picLine";
+            this.picLine.Size = new System.Drawing.Size(116, 21);
+            this.picLine.TabIndex = 1;
+            this.picLine.TabStop = false;
+            this.picLine.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picLine_MouseDown);
+            this.picLine.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picLine_MouseMove);
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.Transparent;
+            this.picBall.Image = ((System.Drawing.Image)(resources.GetObject("picBall.Image")));
+            this.picBall.Location = new System.Drawing.Point(661, 89);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(62, 51);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.button1);
+            this.playground.Controls.Add(this.lblScore);
+            this.playground.Controls.Add(this.lbl);
+            this.playground.Controls.Add(this.picLine);
+            this.playground.Controls.Add(this.picBall);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(1029, 551);
+            this.playground.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(927, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "重新";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblScore.Location = new System.Drawing.Point(85, 13);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(31, 35);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "0";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbl.Location = new System.Drawing.Point(3, 9);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(88, 40);
+            this.lbl.TabIndex = 2;
+            this.lbl.Text = "分數:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 551);
+            this.Controls.Add(this.playground);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "打球球";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picLine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.playground.ResumeLayout(false);
+            this.playground.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox picLine;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 視窗-擊球/Final_project/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_project
+{
+    public partial class Form1 : Form
+    {
+        public int x = 4; //球水平移動的速度
+        public int y = 4; //球垂直移動的速度
+        int mdx; //滑鼠點的地方
+        int score = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            timer.Start();           
+        }
+        private void picLine_MouseDown(object sender, MouseEventArgs e) //滑鼠只能水平移動
+        {
+            mdx = e.X;
+        }
+        private void picLine_MouseMove(object sender, MouseEventArgs e) //板子的拖曳範圍
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int Lx = picLine.Left + (e.X - mdx);
+
+                if (Lx < 0)
+                    Lx = 0; //不超過左邊框
+
+                if (Lx > this.ClientSize.Width - picLine.Width) //不超過右邊框
+                    Lx = this.ClientSize.Width - picLine.Width;
+                picLine.Left = Lx;
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            picBall.Left += x;
+            picBall.Top += y;
+            Random r = new Random(); //顏色隨機變換
+            if (picBall.Bottom > picLine.Top) 
+            {
+                int c = (picBall.Left + picBall.Right) / 2;
+                if (c >= picLine.Left && c <= picLine.Right)
+                {
+                    x += 2;
+                    y += 2;
+
+                    y = -y; //反彈
+                    x = -x;
+                   
+                    playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+                    score++;
+                    lblScore.Text = score.ToString();
+                }
+            }  
+            if (picBall.Left <= playground.Left)
+            { 
+                x = - x;
+                playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+            }
+            if (picBall.Top <= playground.Top)
+            {
+                y = -y;
+                playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+            }
+            if (picBall.Right >= playground.Right)
+            {
+                x = -x;
+                playground.BackColor = Color.FromArgb(r.Next(150, 255), r.Next(150, 255), r.Next(150, 255));
+            }
+            
+            if ((picBall.Top + picBall.Height) >= playground.Bottom)  //跑到視窗外，就結束
+            {
+                timer.Stop();
+                MessageBox.Show("遊戲結束");
+            }           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picBall.Top = 100;
+            picBall.Left = 50;
+            score = 0;
+            lblScore.Text = "0";
+            timer.Start();
+        }
+    }
+}
