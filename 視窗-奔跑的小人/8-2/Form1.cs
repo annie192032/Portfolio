25289560435354a0d584e35af37ad77cc3d06fd8
annie192032 <39673275+annie192032@users.noreply.github.com>
2020-03-23 17:37:14 +0800

    視窗程式

---
+++ 視窗-奔跑的小人/8-2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_2
+{
+    public partial class Form1 : Form
+    {
+        int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageList1.Images[index];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left -= 3;
+            if (pictureBox1.Left < 0) pictureBox1.Left = this.Size.Width;
+
+            index++;
+            if (index >= imageList1.Images.Count) index = 0;
+
+            pictureBox1.Image = imageList1.Images[index];
+        }
+
+        private void hScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            timer1.Interval = 100 - hScrollBar1.Value;
+        }
+    }
+}
