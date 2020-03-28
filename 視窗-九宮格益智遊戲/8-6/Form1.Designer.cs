ae23e798bfe430b0783614b3c2cfa77a495a5c9b
annie192032 <39673275+annie192032@users.noreply.github.com>
2020-03-29 01:43:55 +0800

    視窗-九宮格益智遊戲

---
+++ 視窗-九宮格益智遊戲/8-6/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace _8_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(215, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(35, 107);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 52);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(123, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(57, 52);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(215, 107);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(57, 52);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(35, 199);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(57, 52);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(123, 199);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(57, 52);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(215, 199);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(57, 52);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // lblTime
+            // 
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTime.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblTime.Location = new System.Drawing.Point(316, 53);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(93, 26);
+            this.lblTime.TabIndex = 9;
+            this.lblTime.Text = "秒數";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCount.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblCount.Location = new System.Drawing.Point(316, 140);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(93, 23);
+            this.lblCount.TabIndex = 10;
+            this.lblCount.Text = "次數";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(71, 279);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(215, 279);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 12;
+            this.btnEnd.Text = "結束";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 327);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "九宮格益智遊戲";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 視窗-九宮格益智遊戲/8-6/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_6
+{
+    public partial class Form1 : Form
+    {
+        Color onColor = Color.Coral; // 將onColor的顏色設為咖啡色
+        Color offColor = Color.Yellow; // 將offColor的顏色設為黃色
+        int seconds, count; //紀錄遊戲時間以及按鍵的次數
+        Button[] Buttons = new Button[10]; //儲存按鈕的陣列(陣列第一個元素沒有用到)
+        int[] btnState = new int[10];   //紀錄每個按鈕的狀態(陣列第一個元素沒有用到)
+        int[,] ChangeCells = {{-1,-1,-1,-1,-1},  //第一列是無用的數值
+                                    {1, 2, 4, 5, -1},  //按鈕1影響按鈕1, 2, 4, 5
+	                   {2, 1, 3, -1, -1}, //按鈕2影響按鈕2, 1, 3
+	                   {3, 2, 5, 6, -1},  //按鈕3影響按鈕3, 2, 5, 6
+	                   {4, 1, 7, -1, -1}, //按鈕4影響按鈕4, 1, 7
+	                   {5, 2, 4, 6, 8},   //按鈕5影響按鈕5, 2, 4, 6, 8
+	                   {6, 3, 9, -1, -1}, //按鈕6影響按鈕6, 3, 9
+	                   {7, 4, 5, 8, -1},  //按鈕7影響按鈕7, 4, 5, 8
+	                   {8, 7, 9, -1, -1}, //按鈕8影響按鈕8, 7, 9
+	                   {9, 5, 6, 8, -1},}; //按鈕9影響按鈕9, 5, 6, 8
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
